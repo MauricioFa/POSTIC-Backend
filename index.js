6f663981c2075d2d517f0a4c0e2fa2b637decfff
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-11-27 19:07:31 -0500

    Access for data customers OK

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 
 const { config } = require('./config/index');
 const productsApi = require('./routes/products');
+const customersApi = require('./routes/customers');
 
 //Middleware Body-parser
 app.use(express.json());
 app.use(cors());
 
 productsApi(app);
+customersApi(app);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
---
+++ routes/customers.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const CustomersService = require('../services/customers');
+
+function customersApi(app) {
+  const customersService = new CustomersService();
+  const router = express.Router();
+  app.use('/api/customers', router);
+
+  router.get('/', async function(req, res, next) {
+    const { tags } = req.query;
+    try {
+      const customers = await customersService.getCustomers({ tags });
+      res.status(200).json(customers);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.get('/:customerId', async function(req, res, next) {
+    const { customerId } = req.params;
+    try {
+      const customers = await customersService.getCustomer({ customerId });
+      res.status(200).json(customers);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async function(req, res, next) {
+    const { body: customer } = req;
+    try {
+      const createdCustomerId = await customersService.createCustomer({ customer });
+      res.status(201).json(createdCustomerId);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:customerId', async function(req, res, next) {
+    const { customerId } = req.params;
+    const { body: customer } = req;
+    try {
+      const updatedCustomerId = await customersService.updateCustomer({
+        customerId,
+        customer,
+      });
+      res.status(200).json(updatedCustomerId);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:customerId', async function(req, res, next) {
+    const { customerId } = req.params;
+    try {
+      const deletedCustomerId = await customersService.deleteCustomer({
+        customerId,
+      });
+      res.status(200).json(deletedCustomerId);
+    } catch (error) {
+      next(error);
+    }
+  });
+}
+
+module.exports = customersApi;
